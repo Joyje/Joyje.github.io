6c2c284f3e12563753b751e14e3341148b578ea0
Joyje <Joel.I.Sahlin@Gmail.com>
2023-09-25 07:41:04 +0200

    Slides with certificates for smaller screens

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+let slideShow = document.currentScript.parentElement;
+let slideIndex = 0;
+let slides;
+ 
+window.onload = function() {
+  slides = document.getElementsByClassName(slideShow.classList[0] + 'Slide');
+}
+
+function changeSlide(direction) {
+  slides[slideIndex].style.visibility = 'hidden';
+  switch(direction) {
+    case 'left':
+      if (slideIndex == 0) slideIndex = slides.length;
+      slides[--slideIndex].style.visibility = 'visible';
+      break;
+    case 'right':
+      if (slideIndex == slides.length-1) slideIndex = -1;
+      slides[++slideIndex].style.visibility = 'visible';
+      break;
+  }
+}
